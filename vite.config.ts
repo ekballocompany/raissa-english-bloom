cd108c050c5426aff9f6489bfed992cdec59fcd9
Breno Silva <121455073+brenosilvaa@users.noreply.github.com>
2025-05-22 17:50:41 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './', // <-- isso garante que os caminhos funcionem corretamente
+  build: {
+    outDir: 'dist', // <-- evita que ele crie subpastas como "arte-corte-identidade-76"
+    emptyOutDir: true, // <-- limpa a pasta dist antes do build
+  },
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
