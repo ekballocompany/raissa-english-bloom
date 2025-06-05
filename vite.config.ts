2c0d1ccc755c8610f52d63ae66e3187c9d2d3b2a
Breno Silva <breno.silva@alcancetech.com.br>
2025-06-05 17:59:20 -0300

    ajuste no vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/raissa-english-bloom/", // Caminho base para GitHub Pages
 
   server: {
     host: "::",
